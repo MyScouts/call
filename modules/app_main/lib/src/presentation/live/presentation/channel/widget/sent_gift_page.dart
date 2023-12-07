675ebeab801faf735f74efc0b05b712d9578720e
Dinh Tien Thinh <ltthinhtb@gmail.com>
2023-12-07 12:22:03 +0700

    fix avatar

--- modules/app_main/lib/src/presentation/live/presentation/channel/widget/sent_gift_page.dart
+++ modules/app_main/lib/src/presentation/live/presentation/channel/widget/sent_gift_page.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:app_core/app_core.dart';
 import 'package:design_system/design_system.dart';
 import 'package:flutter/material.dart';
 import 'package:imagewidget/imagewidget.dart';
@@ -354,19 +355,12 @@ class _GiftWidgetState extends State<GiftWidget> with TickerProviderStateMixin {
                             ),
                           ),
                           child: ClipOval(
-                            child: widget.gift.giver?.avatar == null
-                                ? ImageWidget(
-                                    ImageConstants.defaultAvatar,
-                                    width: 53,
-                                    height: 53,
-                                    fit: BoxFit.fill,
-                                  )
-                                : ImageWidget(
-                                    widget.gift.giver?.avatar ?? '',
-                                    width: 53,
-                                    height: 53,
-                                    fit: BoxFit.cover,
-                                  ),
+                            child: ImageWidget(
+                              widget.gift.giver.getAvatar,
+                              width: 53,
+                              height: 53,
+                              fit: BoxFit.fill,
+                            ),
                           ),
                         ),
                       ),
