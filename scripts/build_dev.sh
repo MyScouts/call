ffa807282d49258040f7665d2d8f9191d51c9c64
Hoàng Huy <53051372+MyScouts@users.noreply.github.com>
2023-12-07 21:20:28 +0700

    feat: build 2.1.0(36)

--- scripts/build_dev.sh
+++ scripts/build_dev.sh
@@ -5,7 +5,7 @@ echo "CLEAN DEPENS !!!"
 cd $(pwd)/scripts && sh clean.sh
 
 echo "CLEAN POD !!!"
-cd $(pwd)/ios  &&  pod deintegrate && pod install
+cd $(pwd)/../ios  &&  pod deintegrate && pod install
 
 echo "BUILD ANDROID !!!"
 flutter build  apk --target lib/main_dev.dart
